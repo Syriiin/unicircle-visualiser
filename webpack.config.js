a479c9f4ac273355ba08d02db44034b1233c60cb
Samuel Cattini-Schultz <syriiins@gmail.com>
2019-01-05 22:26:34 +1100

    Setup webpack plugins to generate single html file output
    This config is required so that the single file can be embedded as a resource in the WPF app executable and loaded into the chromium frame from memory rather than a filesystem.
    Another webpack config will be made later to output the component as a library or something.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
     entry: './src/index.ts',
     devtool: 'inline-source-map',
+    mode: 'development',
     module: {
         rules: [
             {
@@ -18,5 +21,12 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inlineSource: '.js$'
+        }),
+        new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+    ]
 };
