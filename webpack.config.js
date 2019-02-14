57f4e2ffc486b7b9678ce0b616e5b0a477848be0
Samuel Cattini-Schultz <syriiins@gmail.com>
2019-02-15 04:40:59 +1100

    Add webpack plugin to clean up output

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const FilterChunkWebpackPlugin = require('filter-chunk-webpack-plugin');
 
 module.exports = {
+    name: 'standalone',
     entry: './src/index.ts',
     devtool: 'inline-source-map',
     mode: 'development',
@@ -27,6 +29,11 @@ module.exports = {
             template: 'src/index.html',
             inlineSource: '.js$'
         }),
-        new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+        new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
+        new FilterChunkWebpackPlugin({
+            patterns: [
+                'bundle.js'
+            ]
+        })
     ]
 };
